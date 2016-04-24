a759a7a2a327aa41e888bf361c4f29fb1414524b
Janne Lönn <janne@izettle.com>
2016-04-24 21:51:25 +0200

    Fixes test accuracy

--- test/index.js
+++ test/index.js
@@ -1,44 +1,51 @@
-import test from 'tape'
+import test from 'tape-catch'
 import sinon from 'sinon'
 import fetchCached from '../'
 
 const URL = 'http://localhost.com'
+const FETCH_DATA = { fetched: 'fetched' }
 const CACHE_JSON = { cached: 'cached' }
 const CACHE_TEXT = 'cached'
-const FETCH_DATA = { fetched: 'fetched' }
+const fetchStub = sinon.stub().withArgs(URL).returns(
+  Promise.resolve({
+    json: () => Promise.resolve(FETCH_DATA),
+    clone: () => {
+      return { text: () => Promise.resolve(JSON.stringify(FETCH_DATA)) }
+    }
+  })
+)
 
 test('returns a function called with new', function (t) {
   t.plan(1)
-  t.is(typeof new fetchCached({ fetch: true, cache: true }), 'function')
+  t.is(typeof new fetchCached({ fetch: {}, cache: {} }), 'function')
 })
 
 test('returns a function called without new', function (t) {
   t.plan(1)
-  t.is(typeof fetchCached({ fetch: true, cache: true }), 'function')
+  t.is(typeof fetchCached({ fetch: {}, cache: {} }), 'function')
 })
 
 test('call without options.cache throws', function (t) {
   t.plan(1)
-  t.throws(() => fetchCached({ fetch: true }))
+  t.throws(() => fetchCached({ fetch: {} }))
 })
 
 test('call without options.fetch throws', function (t) {
   t.plan(1)
-  t.throws(() => fetchCached({ cache: true }))
+  t.throws(() => fetchCached({ cache: {} }))
 })
 
 test('call with options.fetch and options.cache does not throws', function (t) {
   t.plan(1)
-  t.doesNotThrow(() => fetchCached({ fetch: true, cache: true }))
+  t.doesNotThrow(() => fetchCached({ fetch: {}, cache: {} }))
 })
 
-test.skip('fetch returns response data if no cache exists', function (t) {
+test('returns response data if no cache exists', function (t) {
   t.plan(1)
 
-  const fetchStub = sinon.stub().returns(Promise.resolve(FETCH_DATA))
   const cacheStub = {
     get: sinon.stub().withArgs(URL).returns(Promise.resolve(null)),
-    set: sinon.spy()
+    set: sinon.stub()
   }
 
   const fetch = fetchCached({
@@ -47,41 +54,42 @@ test.skip('fetch returns response data if no cache exists', function (t) {
   })
 
   fetch(URL)
-    .then(json =>  t.deepEqual(json, FETCH_DATA))
+    .then(response => response.json())
+    .then(json => t.equal(json, FETCH_DATA))
 })
 
-test('fetch returns .json() cached data if cache exists', function (t) {
+test('returns .json() cached data if cache exists', function (t) {
   t.plan(1)
 
-  const fetchStub = sinon.stub().returns(Promise.resolve(FETCH_DATA))
   const cacheStub = {
     get: sinon.stub().returns(Promise.resolve(JSON.stringify(CACHE_JSON))),
     set: sinon.stub()
   }
+
   const fetch = fetchCached({
     fetch: fetchStub,
     cache: cacheStub
   })
 
   fetch(URL)
-    .then((response) => { return response.json() })
-    .then((json) => { t.deepEqual(json, CACHE_JSON) })
+    .then(response => response.json())
+    .then(json => t.deepEqual(json, CACHE_JSON))
 })
 
-test('fetch returns .text() cached data if cache exists', function (t) {
+test('returns .text() cached data if cache exists', function (t) {
   t.plan(1)
 
-  const fetchStub = sinon.stub().returns(Promise.resolve(FETCH_DATA))
   const cacheStub = {
     get: sinon.stub().returns(Promise.resolve(CACHE_TEXT)),
     set: sinon.stub()
   }
+
   const fetch = fetchCached({
     fetch: fetchStub,
     cache: cacheStub
   })
 
   fetch(URL)
-    .then((response) => { return response.text() })
-    .then((json) => { t.deepEqual(json, CACHE_TEXT) })
+    .then(response => response.text())
+    .then(text => t.deepEqual(text, CACHE_TEXT))
 })
