44ba2d8fb732a19a37c49cb5d90d0f5a78f5b179
Janne Lönn <janne@izettle.com>
2016-06-01 21:40:11 +0200

    Refactor without function changes

--- index.js
+++ index.js
@@ -8,25 +8,26 @@ export default function fetchCached(options) {
 
   const { fetch, cache } = options
 
-  function setCache(key, data) {
-    data.then(value => { cache.set(key, value) })
+  function cachedResponse(data) {
+    if (!data) return null
+
+    return Promise.resolve({
+      ok: true,
+      url: key,
+      status: 200,
+      statusText: 'OK',
+      json: () => Promise.resolve(JSON.parse(data)),
+      text: () => Promise.resolve(data)
+    })
   }
 
-  function getCache(key) {
-    const value = cache.get(key).then(data => {
-      if (!data) return null
+  function cachingRequest(url, options) {
+    return fetch(url, options)
+      .then(response => {
+        response.clone().text().then(value => cache.set(url, value))
 
-      return Promise.resolve({
-        ok: true,
-        url: key,
-        status: 200,
-        statusText: 'OK',
-        json: () => Promise.resolve(JSON.parse(data)),
-        text: () => Promise.resolve(data)
+        return Promise.resolve(response)
       })
-    })
-
-    return value
   }
 
   return function cachedFetch(url, options = {}) {
@@ -35,19 +36,14 @@ export default function fetchCached(options) {
       return fetch(url, options)
     }
 
-    const response = getCache(url)
+    return cache.get(key)
+      .then(cachedResponse)
       .then(cached => {
         // return the cached result if it exist
         if(cached) return cached
 
-        // return fetch for non-cached requests and set cache
-        return fetch(url, options).then(response => {
-          setCache(url, response.clone().text())
-
-          return Promise.resolve(response)
-        })
+        // return fetch request after setting cache
+        return cachingRequest(url, options)
       })
-
-    return response
   }
 }
